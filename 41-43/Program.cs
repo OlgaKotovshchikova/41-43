7554818cfa437ec51cbc001169a64d59a13eb732
Olga <olchiknega@mail.ru>
2022-08-24 22:01:30 +0700

    all tasks done

--- 41-43/Program.cs
+++ 41-43/Program.cs
@@ -44,15 +44,33 @@ namespace _41_43
 
             #region Задача 43
             /*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
-             * y = k1 * x + b1, y = k2 * x + b2; 
+             * y = k1 * x + b1
+             * y = k2 * x + b2; 
              * значения b1, k1, b2 и k2 задаются пользователем.
-             * b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)*/
+             * b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0,5; -0,5)*/
+
+           // k1 и k2 - угловые коэф.
+           // b1 и b2 - пересечение прямых с осью y
 
             static void TaskFourtythree()
             {
+                Console.Write("Введите угловой коэффициент первой прямой (k1): ");
+                double k1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите пересечение первой прямой с осью y (b1): ");
+                double b1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите угловой коэффициент второй прямой (k2): ");
+                double k2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите пересечение второй прямой с осью y (b2): ");
+                double b2 = Convert.ToDouble(Console.ReadLine());
 
+                if ((k1 == k2) & (b1 != b2)) Console.WriteLine("Прямые различны и не пересекаются");
+                if ((k1 == k2) & (b1 == b2)) Console.WriteLine("Прямые совпадают");
+                if (k1 == k2) Console.WriteLine("Прямые параллельны");
+                
+                double x = (b2 - b1) / (k1 - k2);
+                double y = k1 * x + b1;
+                Console.WriteLine("("+$"{x}; "+$"{y}"+")");
             }
-
             #endregion
         }
     }
